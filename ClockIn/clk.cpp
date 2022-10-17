9fb6f2943a2c0bdf506dcfc9c4a74cb0341289d3
William Simpson <VyProductions.music@gmail.com>
2022-10-17 15:55:17 -0700

    Added NCurses interface.

--- ClockIn/clk.cpp
+++ ClockIn/clk.cpp
@@ -1,12 +1,14 @@
 #include <chrono>
 #include <cstring>
 #include <ctime>
+#include <curses.h>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <thread>
 #include <unordered_map>
+#include <vector>
 
 #include <ansi.hpp>
 
@@ -16,12 +18,11 @@
 
 using namespace std::chrono_literals;
 using namespace ansi;
-using namespace std;
 
-void clear(ostream& output);
-istream& operator>>(istream& input, DynamicArray<alarm_t>& alarms);
-istream& operator>>(istream& input, alarm_t& alarm);
-ostream& operator<<(ostream& output, alarm_t& alarm);
+void clear(std::ostream& output);
+std::istream& operator>>(std::istream& input, DynamicArray<alarm_t>& alarms);
+std::istream& operator>>(std::istream& input, alarm_t& alarm);
+std::ostream& operator<<(std::ostream& output, alarm_t& alarm);
 
 bool operator==(alarm_t lhs, alarm_t rhs);
 bool operator<(alarm_t lhs, alarm_t rhs);
@@ -29,8 +30,12 @@ bool operator<(alarm_t lhs, alarm_t rhs);
 void validate(alarm_t& alarm);
 void get_dayname(char day[4], const alarm_t& alarm);
 void get_monthname(char mon[4], int month);
+void schedule_output();
+void menu_prompt();
+void input_handler();
+void update();
 
-unordered_map<int, int> month_days {
+std::unordered_map<int, int> month_days {
     {1, 31},
     {2, 28},
     {3, 31},
@@ -45,7 +50,7 @@ unordered_map<int, int> month_days {
     {12, 31}
 };
 
-unordered_map<int, const char*> day_names {
+std::unordered_map<int, const char*> day_names {
     {0, "Sun"},
     {1, "Mon"},
     {2, "Tue"},
@@ -56,7 +61,7 @@ unordered_map<int, const char*> day_names {
     {7, "N/A"}
 };
 
-unordered_map<int, const char*> month_names {
+std::unordered_map<int, const char*> month_names {
     {1, "Jan"},
     {2, "Feb"},
     {3, "Mar"},
@@ -72,110 +77,86 @@ unordered_map<int, const char*> month_names {
     {13, "N/A"}
 };
 
-int main() {
-    ifstream schedule("schedule.txt");
-    ostringstream output;
-    istringstream res_stream;
-    char c;
-    char mon[4];
-    alarm_t res_base;
-    int ctr = 0;
+long input_keyPressed;
 
-    clear(cout);
+#define K_UP 1792833  // Up Arrow
+#define K_DN 1792834  // Down Arrow
+#define K_RT 1792835  // Right Arrow
+#define K_LT 1792836  // Left Arrow
+#define QUIT 113      // q
+#define ENTR 10       // Return
+
+int state;
+
+#define MENU 0  // Program Is At The Main Menu
+#define SCHD 1  // Program Is Viewing The Scheduled Alarms List
+#define SCED 2  // Program Is Editing The Scheduled Alarms List
+#define TIMR 3  // Program Is Viewing The Timer
+#define TIED 4  // Program Is Editing The Timer
+#define WCLK 5  // Program Is Viewing The World Clock List
+#define WCED 6  // Program Is Editing The World Clock List
+#define EXIT 7  // Program Is Exiting
+
+int running = 1;
+
+int selection;
+
+#define S_OPT 0
+#define T_OPT 1
+#define W_OPT 2
+#define E_OPT 3
+
+std::ifstream schedule("schedule.txt");
+std::ostringstream output;
+std::istringstream res_stream;
+char c;
+char mon[4];
+alarm_t res_base;
+DynamicArray<alarm_t> alarms;
+
+int main() {
+    clear(std::cout);
+
+    initscr();
+    noecho();
+    cbreak();
+    curs_set(0);
+    refresh();
 
     // verify schedule file exists
     if (!schedule.is_open()) {
-        cout << cyan << "No schedule file exists.\nCreating a new one..."
-                  << reset << endl;
-        ofstream new_schedule("schedule.txt");
+        std::cout << cyan << "No schedule file exists.\r\nCreating a new one..."
+                  << reset << std::endl;
+        std::ofstream new_schedule("schedule.txt");
         new_schedule.close();
         schedule.open("schedule.txt");
     }
-
-    DynamicArray<alarm_t> alarms;
     schedule >> alarms;
 
-#ifndef DEBUG
-    this_thread::sleep_for(5000ms);
+    std::this_thread::sleep_for(1000ms);
+    
+    clear();
 
-    while (true) {
-#endif
-        auto now = time(nullptr);
-        char* res = asctime(localtime(&now));
+    // prompt user with menu
+    selection = 0;
+    menu_prompt();
 
-        res_stream.str(res);
-        res_stream.ignore(4);
-        res_stream.get(mon, 4);
-        res_stream >> res_base.day >> res_base.hour >> c >> res_base.minute
-                   >> c >> res_base.second >> res_base.year;
-        for (auto [key, value] : month_names) {
-            if (!strcmp(value, mon)) {
-                res_base.month = key;
-            }
-        }
+    // print schedules
+    // schedule_output();
 
-        output << setw(26) << "Current Schedule\n";
-        output << right;
-
-        for (auto p = alarms.begin(); p != alarms.end(); ++p) {
-            char day[4] {};
-            char mon[4] {};
-
-            get_dayname(day, *p);
-            get_monthname(mon, p->month);
-
-            output << setfill('0') << "     ";
-
-            if (res_base < *p) {
-                output << bg_blue;
-            } else if (res_base == *p && p->hit == false) {
-                output << bg_green << '\a';
-                p->hit = true;
-            } else if (res_base == *p) {
-                output << bg_green;
-            } else {
-                output << bg_red;
-                p->hit = true;
-            }
-
-            output << day << ' ' << mon << ' ' << setw(2) << p->day << ' '
-                   << setw(2) << p->hour << ':' << setw(2) << p->minute << ':'
-                   << setw(2) << p->second << ' ' << p->year
-                   << setfill(' ') << reset << setw(30)
-                   << (res_base == *p ? yellow :
-                       res_base <  *p ? reset  : dk_gray) << p->desc
-                   << reset << '\n';
-        }
-
-        output << '\n' << setw(24) << "Current Date\n";
-
-        output << "     ";
-        int i = 0;
-        while (res[i] != '\n') {
-            output.put(res[i++]);
-        }
-        output << setw(12) << ctr++;
-
-        cout << output.str() << reset << flush;
-        output.str("");
-
-        this_thread::sleep_for(100ms);
-#ifndef DEBUG
-        clear(cout);
-    }
-#endif
+    endwin();
 
     return 0;
 }
 
-void clear(ostream& output) {
+void clear(std::ostream& output) {
     output << "\033[2J\033[1;1H";
 }
 
-istream& operator>>(istream& input, DynamicArray<alarm_t>& alarms) {
+std::istream& operator>>(std::istream& input, DynamicArray<alarm_t>& alarms) {
     alarm_t current;
 
-    printf("Reading schedules...\n");
+    printf("Reading schedules...\r\n");
 
     while (input >> current && current.valid) {
         if (current.valid) {
@@ -184,20 +165,20 @@ istream& operator>>(istream& input, DynamicArray<alarm_t>& alarms) {
     }
 
     if (current == alarm_t{}) {
-        cout << "No alarms on schedule.\n";
+        printf("No alarms on schedule.\r\n");
     } else if ((!input.eof() && input.fail()) || !current.valid) {
-        printf("Invalid alarm syntax on alarm #%ld.\n", alarms.size() + 1);
+        printf("Invalid alarm syntax on alarm #%ld.\r\n", alarms.size() + 1);
     }
 
     return input;
 }
 
-istream& operator>>(istream& input, alarm_t& alarm) {
+std::istream& operator>>(std::istream& input, alarm_t& alarm) {
     char c;
     input >> alarm.month >> c >> alarm.day >> c >> alarm.year
           >> alarm.hour >> c >> alarm.minute >> c >> alarm.second;
     input.ignore(1);
-    getline(input, alarm.desc);
+    std::getline(input, alarm.desc);
 
     if (input) {
         validate(alarm);
@@ -206,7 +187,7 @@ istream& operator>>(istream& input, alarm_t& alarm) {
     return input;
 }
 
-ostream& operator<<(ostream& output, alarm_t& alarm) {
+std::ostream& operator<<(std::ostream& output, alarm_t& alarm) {
     return output << alarm.month << '-' << alarm.day << '-' << alarm.year << ' '
           << alarm.hour << ':' << alarm.minute << ':' << alarm.second;
 }
@@ -258,10 +239,10 @@ void validate(alarm_t& alarm) {
         alarm.second >= 0 && alarm.minute < 60
     ) {
         alarm.valid = true;
-        printf("Alarm #%ld loaded successfully.\n", alarm.id);
+        printf("Alarm #%ld loaded successfully.\r\n", alarm.id);
     } else {
         alarm.valid = false;
-        printf("Alarm #%ld failed to load.\n", alarm.id);
+        printf("Alarm #%ld failed to load.\r\n", alarm.id);
     }
 }
 
@@ -301,4 +282,143 @@ void get_monthname(char mon[4], int month) {
             default: mon[i] = month_names[month][i]; break;
         }
     }
+}
+
+void schedule_output() {
+    while (true) {
+        auto now = time(nullptr);
+        char* res = asctime(localtime(&now));
+
+        res_stream.str(res);
+        res_stream.ignore(4);
+        res_stream.get(mon, 4);
+        res_stream >> res_base.day >> res_base.hour >> c >> res_base.minute
+                   >> c >> res_base.second >> res_base.year;
+        
+        for (auto [key, value] : month_names) {
+            if (!strcmp(value, mon)) {
+                res_base.month = key;
+            }
+        }
+
+        output << std::setw(26) << "Current Schedule\r\n";
+        output << std::right;
+
+        for (auto p = alarms.begin(); p != alarms.end(); ++p) {
+            char day[4] {};
+            char mon[4] {};
+
+            get_dayname(day, *p);
+            get_monthname(mon, p->month);
+
+            output << std::setfill('0') << "     ";
+
+            if (res_base < *p) {
+                output << bg_blue;
+            } else if (res_base == *p && p->hit == false) {
+                output << bg_green << '\a';
+                p->hit = true;
+            } else if (res_base == *p) {
+                output << bg_green;
+            } else {
+                output << bg_red;
+                p->hit = true;
+            }
+
+            output << day << ' ' << mon << ' ' << std::setw(2) << p->day << ' '
+                   << std::setw(2) << p->hour << ':' << std::setw(2) << p->minute << ':'
+                   << std::setw(2) << p->second << ' ' << p->year
+                   << std::setfill(' ') << reset << std::setw(30)
+                   << (res_base == *p ? yellow :
+                       res_base <  *p ? reset : dk_gray) << p->desc
+                   << reset << "\r\n";
+        }
+
+        output << '\n' << std::setw(24) << "Current Date\r\n";
+
+        output << "     ";
+        int i = 0;
+        while (res[i] != '\n') {
+            output.put(res[i++]);
+        }
+
+        std::cout << output.str() << reset << std::flush;
+        output.str("");
+
+        std::this_thread::sleep_for(100ms);
+        clear(std::cout);
+    }
+}
+
+void menu_prompt() {
+    state = MENU;
+
+    clear();
+    refresh();
+    printf("[%c] Schedule\r\n", (selection == 0 ? 'X' : ' '));
+    printf("[%c] Timer\r\n", (selection == 1 ? 'X' : ' '));
+    printf("[%c] World Clock\r\n", (selection == 2 ? 'X' : ' '));
+    printf("[%c] Exit\r\n", (selection == 3 ? 'X' : ' '));
+
+    input_handler();
+}
+
+void input_handler() {
+    char ch[4] {};
+    if (state != EXIT) {
+        input_keyPressed = -1;
+        ch[0] = getch();
+        if (ch[0] == '\033') {
+            ch[1] = getch();
+            ch[2] = getch();
+
+            switch (ch[2]) {
+            case 'A': input_keyPressed = '\033[A'; break;
+            case 'B': input_keyPressed = '\033[B'; break;
+            case 'C': input_keyPressed = '\033[C'; break;
+            case 'D': input_keyPressed = '\033[D'; break;
+            }
+        } else {
+            input_keyPressed = (long)ch[0];
+        }
+        update();
+    }
+}
+
+void update() {
+    switch (state) {
+    case MENU:
+        switch (input_keyPressed) {
+        case K_DN:
+            if (selection < 3) ++selection;
+            menu_prompt();
+            break;
+        case K_UP:
+            if (selection > 0) --selection;
+            menu_prompt();
+            break;
+        case ENTR:
+            switch (selection) {
+            case S_OPT:
+                state = SCHD;
+                break;
+            case T_OPT:
+                state = TIMR;
+                break;
+            case W_OPT:
+                state = WCLK;
+                break;
+            case E_OPT:
+                state = EXIT;
+                break;
+            default:
+                break;
+            }
+            break;
+        default:
+            printf("Unknown action: %ld\r\n", input_keyPressed);
+            break;
+        }
+        break;
+    };
 }
